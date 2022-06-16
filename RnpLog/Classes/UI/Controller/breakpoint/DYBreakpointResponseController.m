168fcafa47210470d746a97d78c758191b0d9038
yangtianyan <yangtianyan@diyiedu.com>
2022-06-16 11:44:07 +0800

    feat: 设置数据时输入优化

--- RnpLog/Classes/UI/Controller/breakpoint/DYBreakpointResponseController.m
+++ RnpLog/Classes/UI/Controller/breakpoint/DYBreakpointResponseController.m
@@ -32,6 +32,7 @@
     self.navigationItem.rightBarButtonItems =@[saveItem] ;
 }
 
+
 - (void)initUI{
     self.textView = UITextViewNew().rnp
     .backgroundColor(UIColor.whiteColor)
@@ -43,6 +44,25 @@
     })
     .view;
     
+}
+#pragma mark -- notification
+- (void)notification{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyBoardDidShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyBoardDidHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)keyBoardDidShow:(NSNotification *)notify{
+    CGRect rect = [notify.userInfo[UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+    [self.textView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.top.mas_equalTo(0);
+        make.bottom.mas_equalTo(-rect.size.height);
+    }];
+}
+
+- (void)keyBoardDidHide:(NSNotification *)notify{
+    [self.textView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.edges.mas_equalTo(0);
+    }];
 }
 #pragma mark -- Action
 - (void)backAct{
@@ -60,6 +80,7 @@
     [super viewDidLoad];
     [self initNav];
     [self initUI];
+    [self notification];
 }
 + (void)showWithDataModel:(RnpDataModel *)dataModel completion:(void(^)(void))completion{
     dispatch_async(dispatch_get_main_queue(), ^{
--- RnpLog/Classes/UI/Controller/breakpoint/RnpRequestSetupController.m
+++ RnpLog/Classes/UI/Controller/breakpoint/RnpRequestSetupController.m
@@ -35,6 +35,25 @@
     .view;
     
 }
+#pragma mark -- notification
+- (void)notification{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyBoardDidShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyBoardDidHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)keyBoardDidShow:(NSNotification *)notify{
+    CGRect rect = [notify.userInfo[UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+    [self.textView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.top.mas_equalTo(0);
+        make.bottom.mas_equalTo(-rect.size.height);
+    }];
+}
+
+- (void)keyBoardDidHide:(NSNotification *)notify{
+    [self.textView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.edges.mas_equalTo(0);
+    }];
+}
 #pragma mark -- Action
 - (void)saveAct{
     if (self.textView.text.length > 0 && self.onSaveBlock) {
@@ -47,6 +66,7 @@
     [super viewDidLoad];
     [self initNav];
     [self initUI];
+    [self notification];
 }
 
 @end
