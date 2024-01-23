165d8d1ae0c67917198477b34a97d26579b172ae
yangtianyan <coderyty@163.com>
2024-01-23 16:36:14 +0800

    fix: 转换请求头中包含非字符串类型

--- RnpLog/Classes/Util/WKWebView/RnpHookAjaxHandler.m
+++ RnpLog/Classes/Util/WKWebView/RnpHookAjaxHandler.m
@@ -55,7 +55,14 @@
         NSError *err = nil;
         request.HTTPBody = [NSJSONSerialization dataWithJSONObject:requestData options:0 error:&err];
     }
-    [request setAllHTTPHeaderFields:requestHeaders];
+    
+    /// 神奇的key、value可能存在number，强行转换一波
+    NSMutableDictionary<NSString *, NSString *> *newRequestHeaders = [NSMutableDictionary new];
+    [requestHeaders enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+        [newRequestHeaders setValue:[NSString stringWithFormat:@"%@", obj] forKey:[NSString stringWithFormat:@"%@",key]];
+    }];
+    
+    [request setAllHTTPHeaderFields:newRequestHeaders];
     __weak typeof(self) weakSelf = self;
     NSURLSessionConfiguration * configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];//创建一个临时会话配置
     NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration delegate:nil delegateQueue:[[NSOperationQueue alloc] init]];
